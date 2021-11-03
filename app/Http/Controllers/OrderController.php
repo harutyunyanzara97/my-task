e3322c7cded7b27cf6b4099a7913686aa7483e9b
harutyunyanzara97 <harutyunyanzara97@gmail.com>
2021-11-03 13:15:14 +0400

    my-task-retail

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function home() {
+        return view('Make-order');
+    }
+    public function order(Request  $request) {
+
+
+        $domain = 'https://superposuda.retailcrm.ru/';
+        $key = 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb';
+
+        $postData = http_build_query(array(
+            'order' => json_encode(array(
+                'order_status' => 'trouble',
+                'order_type' => 'fizik',
+                'shop' => 'test',
+                'registration_method' => 'test',
+                'order_number' => '4031997',
+                'full_name' => $request->fullname,
+                'client_comment' => $request->comment
+
+)),
+    'apiKey' => $key,
+));
+
+$opts = array('http' =>
+    array(
+        'method' => 'POST',
+        'header' => 'Content-type: application/x-www-form-urlencoded',
+        'content' => $postData
+    )
+);
+
+$getOpts = array('http' =>
+            array(
+                'method' => 'GET',
+                'header' => 'Content-type: application/x-www-form-urlencoded',
+            )
+        );
+
+$newContext = stream_context_create($getOpts);
+
+        $result = json_decode(
+            file_get_contents(
+                $domain . 'api/v5/orders/create?filter[properties][vendor_code]=$request->vendor_code&filter[properties][brand]=$request->brand',
+                false,
+                $newContext
+            ),
+            true);
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', [OrderController::class, 'home'])->name('home');
+Route::post('/order', [OrderController::class, 'order'])->name('order');
+
